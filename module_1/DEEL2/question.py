a742fb95aaed37cae62ca48bcb255c4920746107
Oskar <99077778@mydavinci.nl>
2023-10-02 13:35:20 +0200

    n

---
+++ module_1/DEEL2/question.py
@@ -0,0 +1,17 @@
+
+
+naam = input('Hallo, wat is je naam? ')
+leeftijd = int(input(f'Goedendag {naam}, hoe oud ben je? '))
+eten = input(f'En als {leeftijd} jarige, wat eet je het liefst? ')
+drinken = input(f'En wat drink je het liefst bij {eten}? ')
+
+
+
+
+import time
+
+time.sleep(1)
+
+print(f'De {leeftijd} jarige {naam} drinkt het liefst {drinken} bij {eten}')
+
+
