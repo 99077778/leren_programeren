c724f42df2335915bcecd6c4e004315d2a0e61b6
Oskar <99077778@mydavinci.nl>
2023-10-02 13:20:41 +0200

    n

---
+++ module_1/DEEL2/person.py
@@ -0,0 +1,19 @@
+naam = input('Voer je Naam in: ')
+adres = input('Voer je Adres in: ')
+postcode = input('Voer je Postcode in: ')
+woonplaats = input('Voer je Woonplaats in: ')
+
+
+
+
+print(' -------------------------------')
+
+print(f'|  Naam      : {naam}')
+print(f'|  Adres     : {adres}')
+print(f'|  Postcode  : {postcode}')
+print(f'|  Woonplaats: {woonplaats}')
+
+print(' -------------------------------')
+
+
+
