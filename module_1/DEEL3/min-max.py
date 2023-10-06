b78ed5983ed20846c4d6a5d5a83a68a588ce3d80
Oskar <99077778@mydavinci.nl>
2023-10-06 14:53:11 +0200

    Min en Max

--- module_1/DEEL3/min-max.py
+++ module_1/DEEL3/min-max.py
@@ -11,3 +11,10 @@ elif a < b:
     min = a
 elif a == b:
     print(f'a en b zijn even groot')
+
+
+
+if a > b:
+    print(f'Het minimum is: {min}')
+elif a < b:
+    print(f'Het maximum is: {max}')
