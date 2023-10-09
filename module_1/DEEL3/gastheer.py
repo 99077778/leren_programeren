fa91b98513ef60c8bfaa58ed3a3b8807ff89c11d
Oskar <99077778@mydavinci.nl>
2023-10-09 15:53:08 +0200

    n

---
+++ module_1/DEEL3/gastheer.py
@@ -0,0 +1,12 @@
+MIJN_NAAM = 'oskar'
+SLB_NAAM = "jouke"
+
+gastheer = input('wie is de gastheer')
+gasten = 0
+drank = 1
+chips = 0
+# print((gasten and chips and drank) or (gastheer > "" and drank))
+if (gastheer == MIJN_NAAM) or ((gasten and chips and drank) or (gastheer and drank)) and gastheer != SLB_NAAM:
+    print('Start the Party')
+else:
+    print('geen feest')
