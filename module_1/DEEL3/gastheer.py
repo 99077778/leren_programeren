908b89df15b6a26ad35ba7d0f27c1f5496d07f18
Oskar <99077778@mydavinci.nl>
2023-10-26 15:23:01 +0200

    n

--- module_1/DEEL3/gastheer.py
+++ module_1/DEEL3/gastheer.py
@@ -14,3 +14,22 @@ else:
     print('No Party')
 
 
+
+
+
+
+# MIJN_NAAM = 'oskar'
+# SLB_NAAM = 'jouke'
+
+# gastheer = input('wie is de gastheer')
+# gasten = 0
+# drank = 1
+# chips = 0
+
+
+# start1 = gastheer == MIJN_NAAM
+# start2 = gasten and chips and drank = true
+# start3 = gastheer !
+
+
+
---
+++ module_1/DEEL3/kaasV2.py
@@ -0,0 +1,45 @@
+
+geelkaas = input('Is de kaas geel? (ja/nee): ').lower()
+
+if geelkaas == 'ja':
+    gaten = input('Zitten er gaten in? (ja/nee): ').lower()
+    if gaten == 'ja':
+        duur = input('Is de kaas belachelijk duur? (ja/nee): ').lower()
+        if duur == 'ja':
+            print('Emmenthaler')
+        elif duur == 'nee':
+            print('Leerdammer')
+        else:
+            print('Ongeldige invoer voor duur')
+    elif gaten == 'nee':
+        steen = input('Is de kaas hard als steen? (ja/nee): ').lower()
+        if steen == 'ja':
+            print('Parmigiano Reggiano')
+        elif steen == 'nee':
+            print('Goudse kaas')
+        else:
+            print('Ongeldige invoer voor steen')
+    else:
+        print('Ongeldige invoer voor gaten')
+elif geelkaas == 'nee':
+    schimmel = input('Heeft de kaas blauwe schimmel (ja/nee): ').lower()
+    if schimmel == 'ja':
+        korst = input('Heeft de kaas korst? (ja/nee): ').lower()
+        if korst == 'ja':
+            print('Blue de Rochbaron')
+        elif korst == 'nee':
+            print('Foume dambert')
+        else:
+            print('Ongeldige invoer voor korst')
+    elif schimmel == 'nee':
+        korst2 = input('Heeft de kaas korst? (ja/nee): ').lower()
+        if korst2 == 'ja':
+            print('Camembert')
+        elif korst2 == 'nee':
+            print('Mozzarella')
+        else:
+            print('Ongeldige invoer voor korst2')
+    else:
+        print('Ongeldige invoer voor schimmel')
+else:
+    print('Ongeldige invoer voor geelkaas')
