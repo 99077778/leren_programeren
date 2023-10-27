7f78efed6045cc5992484a8370c311a09cb531c3
Oskar <99077778@mydavinci.nl>
2023-10-27 12:59:34 +0200

    n

---
+++ module_1/DEEL3/min-maxV2.py
@@ -0,0 +1,23 @@
+a = int(input('voer een getal in voor A: '))
+b = int(input('voer een getal in voor B: '))
+
+MAX = 0
+MIN = 0
+
+if a > b:
+    print(f'A is het grootste getal: ')
+    MAX = a
+elif a < b:
+    print(f'A is het kleinste getal: ')
+    MIN = a
+else:
+    print(f'A en B zijn even groot')
+
+
+
+# if a > b:
+#     print(f'Het minimum is: {min}')
+# elif a < b:
+#     print(f'Het maximum is: {max}')
+
+
