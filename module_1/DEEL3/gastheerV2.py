a7ecff1b29c682454decc023696e8f781cd71b88
Oskar <99077778@mydavinci.nl>
2023-10-30 13:42:28 +0100

    n

--- module_1/DEEL3/gastheerV2.py
+++ module_1/DEEL3/gastheerV2.py
@@ -6,11 +6,14 @@ gasten = 0
 drank = 1
 chips = 0
 
-start1 = gastheer == MIJN_NAAM
-start2 = gasten and chips and drank
-start3 = gastheer != SLB_NAAM
+start_mijnnaam = gastheer == MIJN_NAAM
+start_gastenchipsdrank = gasten and chips and drank 
+start_gastheerdrank = gastheer and drank
+start_slbnaam = gastheer != SLB_NAAM
 
-if (start1 or (start2 or (gastheer and drank)) and start3):
+
+
+if (start_mijnnaam or (start_gastenchipsdrank or (start_gastheerdrank)) and start_slbnaam):
     print('Start the Party')
 else:
     print('No Party')
--- module_1/DEEL3/minimaalmaximaalgasten.py
+++ module_1/DEEL3/minimaalmaximaalgasten.py
@@ -1,22 +1,23 @@
 MIJN_NAAM = 'oskar'
-SLB_NAAM = "jouke"
+SLB_NAAM = 'jouke'
 
 gastheer = input('Wie is de gastheer: ').lower()
 
 gasten = int(input('Hoeveel gasten: '))
 
-drank = 0
+drank = 1
 chips = 0
 
 
-start1 = gastheer == MIJN_NAAM
-start2 = gasten and chips and drank
-start3 = gastheer and drank
-start4 = gastheer != SLB_NAAM or 20 >= gasten >=4
+start_mijnnaam = gastheer == MIJN_NAAM
+start_gastnchipsdrank = gasten and chips and drank
+start_gastheerdrank = gastheer and drank
+start_gastheerSLB = gastheer != SLB_NAAM
+start_aantalgasten = 20 >= gasten >=4
 
 
 
-if (start1) or ((start2) or (start3)) and start4:
+if (start_mijnnaam) or ((start_gastnchipsdrank) or (start_gastheerdrank)) and (start_gastheerSLB and start_aantalgasten):
     print('Start the Party')
 else:
 
