f1d06c11ed7dab0ef67f5ff9b3f46190fd9a8898
Oskar <99077778@mydavinci.nl>
2023-11-03 15:14:05 +0100

    update voor try exept

--- module_1/DEEL2/pizzaCalculator.py
+++ module_1/DEEL2/pizzaCalculator.py
@@ -10,35 +10,32 @@ bestellingen = []
 while True:
     try:
         groote = int(input('Welke pizza wil je, kies het nummer: [1] small, [2] medium, [3] large : '))
-        
+        if groote < 1 or groote > 3:
+            raise ValueError("Ongeldige keuze. Kies een nummer tussen 1 en 3.")
     except ValueError as e:
         print(f"Fout: {e}")
-      
-      
-        #if groote < 1 or groote > 3:
-            #raise ValueError("Ongeldige keuze. Kies een nummer tussen 1 en 3.")
-   
-        
-        if groote == 1:
-            pizza = "small"
-            prijs = 7.99
-        elif groote == 2:
-            pizza = "medium"
-            prijs = 9.99
-        elif groote == 3:
-            pizza = "large"
-            prijs = 12.99
-        
-        bestellingen.append((pizza, prijs))
-        
-        print(f"De prijs van de gekozen pizza ({pizza}) is: €{prijs:.2f}")
-        
+        continue
 
-        #Nog een pizza bestellen
-        bestel_opnieuw = input("Wil je nog een pizza bestellen? (ja/nee): ").strip().lower()
-        if bestel_opnieuw != 'ja':
-            break
- 
+    if groote == 1:
+        pizza = "small"
+        prijs = 7.99
+    elif groote == 2:
+        pizza = "medium"
+        prijs = 9.99
+    elif groote == 3:
+        pizza = "large"
+        prijs = 12.99
+    
+    bestellingen.append((pizza, prijs))
+    
+    print(f"De prijs van de gekozen pizza ({pizza}) is: €{prijs:.2f}")
+    
+
+    #Nog een pizza bestellen
+    bestel_opnieuw = input("Wil je nog een pizza bestellen? (ja/nee): ").strip().lower()
+    if bestel_opnieuw != 'ja':
+        break
+    
 
 
 #Bestellijst
