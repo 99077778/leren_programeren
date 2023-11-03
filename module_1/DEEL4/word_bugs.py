e25ef36eba4d9a4b3abf37d486ec4df9c100fcb8
Oskar <99077778@mydavinci.nl>
2023-11-03 13:10:24 +0100

    n

--- module_1/DEEL4/word_bugs.py
+++ module_1/DEEL4/word_bugs.py
@@ -1,9 +1,12 @@
-a = str(print('Geef een woord'))
-b = str(print('Geef nog een woord'))
 
-if a > b:     
+
+a = str(input('Geef een woord'))
+b = str(input('Geef nog een woord'))
+
+
+if len(a) > len(b):     
     print('Woord 1 heeft meer letters dan woord 2')
-elif a < b:   
+elif len(a) < len(b):   
     print('Woord 1 heeft minder letters dan woord 2')
 else:                   
     print('Woord 1 en woord 2 hebben evenveel letters')
