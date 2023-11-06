09a170a1e4473f1b5ca5276dd48ae2153c62e312
Oskar <99077778@mydavinci.nl>
2023-11-06 14:41:02 +0100

    n

---
+++ module_1/DEEL5/metenisweten.py
@@ -0,0 +1,27 @@
+
+def nummers(nr1: int, nr2: int) -> str:
+    if nr1 == nr2:
+        return'Allebij de getallen zijn even groot'
+    elif nr1 > nr2:
+        return f'Maximum = {nr1}, minimum = {nr2}'
+    else:
+        return f'Maximum = {nr2}, minimum = {nr1}'
+ 
+ 
+  
+nr1 = int(input("Voer een nummer voor getal 1 in: "))
+nr2 = int(input("Voer een nummer voor getal 2 in: "))
+ 
+
+MIN = 0
+MAX = 0
+ 
+
+ 
+resultaat = nummers(nr1, nr2)
+print(resultaat)
+ 
+ 
+ 
+ 
+ 
