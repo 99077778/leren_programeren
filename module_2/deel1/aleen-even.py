b2e70bfcad34cfe51c52b3e6312d55abe3292528
Oskar <99077778@mydavinci.nl>
2023-12-11 11:12:39 +0100

    n

--- module_2/deel1/aleen-even.py
+++ module_2/deel1/aleen-even.py
@@ -1,4 +1,12 @@
 
+# for getal in range(20, 51):
+#     if getal / 2 == int:
+#         print(getal)
+#     elif getal / 2 == float:
+#         pass
+    
+    
+
 for getal in range(20, 51, 2):
     print(getal)
     
