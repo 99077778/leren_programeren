f410c2a51985697d6ad4709424f5877485fcf49a
Oskar <99077778@mydavinci.nl>
2024-02-02 13:58:31 +0100

    n

---
+++ module_2/deel1/robotarm-python-2023-main/opdarcht12.py
@@ -0,0 +1,23 @@
+from RobotArm import RobotArm
+
+robotArm = RobotArm('exercise 12')
+
+hoveel = 9
+
+for n in range(9):
+    robotArm.grab()
+    colour = robotArm.scan()
+    if colour == 'red':
+        for _ in range(hoveel):
+            robotArm.moveRight()
+        robotArm.drop()
+        hoveel -= 1
+        for _ in range(hoveel):
+            robotArm.moveLeft()
+        
+    else:
+        robotArm.drop()
+        robotArm.moveRight()
+        hoveel -= 1
+
+robotArm.wait()
--- module_2/deel1/robotarm-python-2023-main/opdracht11.py
+++ module_2/deel1/robotarm-python-2023-main/opdracht11.py
@@ -14,12 +14,35 @@ robotArm = RobotArm('exercise 11')
 
 
 
-for _ in range(9):
-    color = robotArm.scan()
-    if color == 'white':
-        robotArm.grab()
-    else:
-        robotArm.moveRight()
 
+
+# for _ in range(9):
+#     color = robotArm.scan()
+#     if color == 'white':
+#         robotArm.grab()
+#     else:
+#         robotArm.moveRight()
+
+
+
+
+
+
+for _ in range(8):
+    robotArm.moveRight()
+
+for n in range(9):
+    robotArm.grab()
+    colour = robotArm.scan()
+    if colour == 'white':
+        robotArm.moveRight()
+        robotArm.drop()
+        if n < 8:
+            robotArm.moveLeft()
+            robotArm.moveLeft()
+    else:
+        robotArm.drop()
+        if n < 8:
+            robotArm.moveLeft()
 
 robotArm.wait()
