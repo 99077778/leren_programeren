a3c6ab47ba748f2f86bd2ec1d00a28054e91e0f5
Oskar <99077778@mydavinci.nl>
2024-02-15 10:21:04 +0100

    updat klubbing

--- module_2/deel4/clubbing.py
+++ module_2/deel4/clubbing.py
@@ -5,36 +5,67 @@ PRIJS_CHAMPAGNE = 12.30
 DRANKJES = ('cola', 'bier', 'champagne')
 VIP_LIST = ('jeroen', 'jouke', 'rudi')
 
-
-
 import time
 
-#bouw hieronder de floowchart na
-
 leeftijd = int(input('Hoe oud ben je?: '))
-bandje = 'rood'
 naam = ''
-verder = 'nee'
+bandje = ''
+stempel = 0
+
+
+probeerlater = f'Probeer het in {18 - leeftijd} jaar nog eens'
+
+complimenten = 'Alstublieft, complimenten van het huis'
+
+jecola = f'Alsjeblieft je cola, dat is dan {PRIJS_COLA} euro'
+jebier = f'Alsjeblieft je bier, dat is dan {PRIJS_BIER} euro'
+jechampagne = f'Alsjeblieft je champagne, dat is dan {PRIJS_CHAMPAGNE} euro'
+
+geenalchol = 'Sorry, je mag geen alcohol bestellen onder de 21'
 
 time.sleep(.5)
 
 if leeftijd < 18:
     print('Sorry je mag niet naar binnen')
-    print(f'Probeer het in {18 - leeftijd} jaar nog eens')
-
+    print(probeerlater)
+    exit()
 else:
     naam = input('Wat is je naam?: ').lower()
 
-
 if naam in VIP_LIST:
-    if leeftijd >=21:
+    if leeftijd >= 21:
         bandje = 'blauw'
+    else:
+        bandje = 'rood'
     print(f'Je krijgt van mij een {bandje} bandje')
-    verder = 'ja'
 else:
-    if leeftijd >=21:
+    if leeftijd >= 21:
         print('Je krijgt van mij een stempel')
-    verder = 'ja'
+        stempel = 1
 
-if verder == 'ja': 
-    print('wat wil je drinken')
+drinken = input('wat wil je drinken?: ').lower()
+
+if drinken == 'cola':
+    if bandje == 'blauw' or bandje == 'rood':
+        print(complimenten)
+    else:
+        print(jecola)
+elif drinken == 'bier':
+    if stempel == 1 or bandje == 'blauw' or bandje == 'rood':
+        if bandje == 'blauw' or bandje == 'rood':
+            print(complimenten)
+        else:
+            print(jebier)
+    else:
+        print(geenalchol)
+        print(probeerlater)
+elif drinken == 'champagne':
+    if bandje == 'blauw':
+        print(jechampagne)
+    elif bandje == 'rood':
+        print(geenalchol)
+        print(probeerlater)
+    else:
+        print('Sorry, alleen vips mogen champagne bestellen')
+else:
+    print('Sorry, geen idee wat je bedoeld, hier een glaasje water')
